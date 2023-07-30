22fa831dbd3c82ae6eef28c7873c32763f22694a
claudiokings Reis <claudio.conceitual@gmail.com>
2023-07-30 18:17:29 +0000

    Exercício 1

--- src/test/java/com/example/AppClaudio.java
+++ src/test/java/com/example/AppClaudio.java
@@ -7,10 +7,20 @@ public class AppClaudio {
         System.out.println();
         System.out.println("minha aplicação em Java\n");
 
-        byte idade = 58;
-        double peso = 84;
+        var idade = 58;
+        var peso = 84.0;
 
         System.out.println("Idade é " + idade + " Peso é " + peso);
+
+        var genero = 'M';
+        System.out.println("Gênero " + genero);
+
+        
+        var necessidades = true;
+        System.out.println("Tem necessidades especiais? " + necessidades);
+
+
+
 
         
     }
