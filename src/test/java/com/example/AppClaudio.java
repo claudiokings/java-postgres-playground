0346143c3e2141409ddcdb4351c19a3c4d329676
claudiokings Reis <claudio.conceitual@gmail.com>
2023-07-30 18:15:06 +0000

    Exercicío 1

---
+++ src/test/java/com/example/AppClaudio.java
@@ -0,0 +1,17 @@
+package com.example;
+
+public class AppClaudio {
+
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println();
+        System.out.println("minha aplicação em Java\n");
+
+        byte idade = 58;
+        double peso = 84;
+
+        System.out.println("Idade é " + idade + " Peso é " + peso);
+
+        
+    }
+}
