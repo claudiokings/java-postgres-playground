b2525db3a9b4706d0df2c7b0f0e913b4b9667de0
claudiokings Reis <claudio.conceitual@gmail.com>
2023-08-21 20:35:41 +0000

    Teste aluno

---
+++ src/main/java/com/example/educacao/dao/AlunoDao.java
@@ -0,0 +1,9 @@
+package com.example.educacao.dao;
+
+public class AlunoDao {
+    public void insert (Aluno aluno) {
+        var sql = "insert into aluno (nome) values = '?'";
+        
+    }
+    
+}
---
+++ src/test/java/com/example/educacao/model/AlunoTest.java
@@ -0,0 +1,10 @@
+package com.example.educacao.model;
+
+import org.junit.jupiter.api.Test;
+
+public class AlunoTest {
+    @Test
+    void CalculaMedia(){
+        
+    }
+}
